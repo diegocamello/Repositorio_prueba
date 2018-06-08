b1a7441feb8ade0081e2c8c277f928a90cf4c4e1
UsuarioN°1000 <UsuarioN1000@192.168.1.86>
2018-06-08 16:32:02 -0400

    commit al repositorio 2

--- src/repositorio_prueba/clase_prueba2.java
+++ src/repositorio_prueba/clase_prueba2.java
@@ -10,5 +10,20 @@ package repositorio_prueba;
  * @author UsuarioN°1000
  */
 public class clase_prueba2 {
-    
+    public void qwer(){
+        System.out.println("repositorio de prueba número 1");
+        System.out.println("repositorio de prueba dedicado al aprendizaje");
+        System.out.println("repositorio de prueba número 1");
+        System.out.println("repositorio de prueba dedicado al aprendizaje");
+        System.out.println("repositorio de prueba número 1");
+        System.out.println("repositorio de prueba dedicado al aprendizaje");
+        System.out.println("repositorio de prueba número 1");
+        System.out.println("repositorio de prueba dedicado al aprendizaje");
+        System.out.println("repositorio de prueba número 1");
+        System.out.println("repositorio de prueba dedicado al aprendizaje");System.out.println("repositorio de prueba número 1");
+        System.out.println("repositorio de prueba dedicado al aprendizaje");System.out.println("repositorio de prueba número 1");
+        System.out.println("repositorio de prueba dedicado al aprendizaje");
+        
+        
+    }
 }
