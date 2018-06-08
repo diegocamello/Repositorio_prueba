ad565ae1ab9c0a8695514b451a5828b8ecc93fb9
UsuarioN°1000 <UsuarioN1000@192.168.1.86>
2018-06-08 16:21:13 -0400

    primer commit al repositorio

---
+++ src/repositorio_prueba/Repositorio_prueba.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repositorio_prueba;
+
+/**
+ *
+ * @author UsuarioN°1000
+ */
+public class Repositorio_prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("repositorio de prueba número 1");
+    }
+    
+}
---
+++ src/repositorio_prueba/clase_prueba2.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repositorio_prueba;
+
+/**
+ *
+ * @author UsuarioN°1000
+ */
+public class clase_prueba2 {
+    
+}
